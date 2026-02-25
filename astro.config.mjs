e38a1afbec119d933195ba1522b6cccdeb51e082
DylanFarkas <dylan.farkas@correounivalle.edu.co>
2026-02-24 19:11:54 -0500

    fix: switch to Vercel adapter and move middleware to src

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,13 @@ import react from '@astrojs/react';
 
 import icon from 'astro-icon';
 
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server', // Modo servidor con SSR por defecto
-  adapter: node({
-    mode: 'standalone'
+  adapter: vercel({
+    webAnalytics: { enabled: true }
   }),
   
   vite: {
